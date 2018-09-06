1e8a6bafda32c13b3099534df1c5301579483f34
wdmleo <leo@wdmstudio.fr>
2018-09-06 09:52:16 +0200

    Ajout ajax

---
+++ Jquery Ajax/ajax.php
@@ -0,0 +1,12 @@
+<?php
+
+// echo "toto;tata;titi";
+
+$arr = [
+    "nom"=>"smith",
+    "prenom"=>"john",
+    "email"=>"smith.john@gmail.com",
+    "password"=>"smith"
+];
+
+echo json_encode($arr);
